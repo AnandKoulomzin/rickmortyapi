f30424584e98dd9e96adde050ccd88b60f8e3945
anandkoulomzin <anand_koulomzin25@milton.edu>
2024-05-16 11:14:12 -0400

    almost done with charactermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CharacterModule } from './character/character.module';
+import { CharactersModule } from './characters/character.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CharacterModule, MongooseModule.forRoot('mongodb+srv://AnandK:AnandK@cluster0.xj5lqs3.mongodb.net/test')],
+  imports: [CharactersModule, MongooseModule.forRoot('mongodb+srv://AnandK:AnandK@cluster0.xj5lqs3.mongodb.net/rickmortyapi')],
   controllers: [AppController],
   providers: [AppService],
 })
